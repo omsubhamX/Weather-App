0a6edbcca18d16d3284b1eaeb2cc08b14195d3f4
Om Subham Sahu <158176222+omsubhamX@users.noreply.github.com>
2024-03-18 14:29:34 +0530

    weatherappcss.js

--- weatherappcss.js
+++ weatherappcss.js
@@ -1,47 +1,6 @@
 
 
-// let searchedWord;
-// let result;
-// // in searchbar we r keeping the html element with id input
-// const searchbar = document.getElementById("input");
-// // we r listening to input event and executing callback func
-// searchbar.addEventListener('input', (event) => {
-//     searchedWord = event.target.value;
-//     // console.log("searchedWord is ", searchedWord);
-// });
 
-// const searchButton = document.getElementById("searchbutton");
-// searchButton.addEventListener('click', async () => {
-//     // Construct URL and options here, using the latest value of searchedWord
-//     const url = `https://yahoo-weather5.p.rapidapi.com/weather?location=${searchedWord}&format=json&u=f`;
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '9fc2531714msha714dfc97f3f700p1080f8jsn3e419d2dd10b',
-//             'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
-//         }
-//     };
-
-//     try {
-//         const response = await fetch(url, options);
-//          result = await response.json();
-//         console.log(result);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// });
-
-
-// // Get the paragraph element by ID
-// const para = document.getElementById("res");
-
-// // Convert the JSON object to a string
-// const resultString = JSON.stringify(result);
-// console.log("Result:", resultString);
-// // Set the text content of the paragraph element to the JSON string
-// para.textContent = resultString;
-
-// // Optional: Log the result to the console for verification
 let searchedWord;
 let result;
 let response;
@@ -73,22 +32,21 @@ searchButton.addEventListener('click', async () => {
     try {
          response = await fetch(url, options);
          result = await response.json();
-        //  const parsedData = JSON.parse(result);
-
+       
         const temperatureincelsius =  convertFahrenheitToCelsius(result.current_observation.condition.temperature); 
      
 const finalTemperature = roundUp(temperatureincelsius)
 if (temperatureincelsius<15) {
-  console.log("its chilling MC");
+  console.log("its chilling ");
 }
 else if (temperatureincelsius>15 && temperatureincelsius<=30) {
 
-  console.log("chaliba mg");
-  message="chaliba mg";
+  console.log("modrate");
+  message="modrate";
 }
 
 else {
-  console.log("bando garam gauchi");
+  console.log("hot");
 }
 
          console.log(temperatureincelsius);
@@ -103,17 +61,10 @@ if (temperatureincelsius<15) {
 }
 
 
-// // Show the weather information container and hide the search bar
-// document.querySelector(".weather").style.display = "block";
-// document.querySelector(".card").style.display = "none";
-
-
-
-// document.getElementById("mainSec").style.display = "block";
 
 document.querySelector("#temperature").innerHTML = finalTemperature + "°C";
 
-    // document.querySelector("#temperature").innerHTML= temperatureincelsius+ "°C";
+   
         console.log(result);
    
         document.querySelector("#windspeed").innerHTML=result.current_observation.wind.speed + "KM/hr";
@@ -122,7 +73,6 @@ document.querySelector("#temperature").innerHTML = finalTemperature + "°C";
         document.querySelector("#location").innerHTML=result.location.city;
 
 
-        // Optional: Log the result to the console for verification
     } catch (error) {
       
         console.error(error);
